017d4793f757c6c60dc45b6adbfbd267c5c4f7fd
inhuman <inhuman.en@gmail.com>
2017-06-24 21:24:56 +0300

    moved to webpack 2, pruned bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -9,64 +9,71 @@ let extractSASS = new ExtractTextPlugin('/styles-build.css');
 let path = require('path');
 
 module.exports = {
-  context: path.join(__dirname),
-  devtool: debug
-    ? "inline-sourcemap"
-    : null,
-  entry: "./client/src/js/main.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            'react', 'es2015', 'stage-0'
-          ],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties']
-        }
-      }, 
-      // {
-      //   test: /\.css$/,
-      //   loader: extractCSS.extract("style-loader", "css-loader")
-      // }, 
-      {
-        test: /\.scss$/,
-        loader: extractSASS.extract("style-loader", "css-loader!sass-loader")
-      }, {
-        test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: "file-loader?name=[name].[ext]"
-      }
-    ]
-  },
-  output: {
-    path: __dirname + "/public/",
-    publicPath: "/",
-    filename: "scripts.min.js"
-  },
-  plugins: debug
-    ? [extractSASS]
-    : [
-      extractSASS,
-      new webpack
-        .optimize
-        .DedupePlugin(),
-      new webpack
-        .optimize
-        .OccurenceOrderPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
-      new webpack
-        .optimize
-        .UglifyJsPlugin({sourcemap: false})
-    ],
-  devServer: {
-    host: "localhost",
-    port: 9000,
-    contentBase: __dirname + "/build"
-  }
+    context: path.join(__dirname),
+    devtool: debug
+        ? "inline-sourcemap"
+        : null,
+    entry: "./client/src/js/main.js",
+    output: {
+        path: __dirname + "/public/",
+        publicPath: "/",
+        filename: "scripts.min.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: [
+                                'react', 'es2015', 'stage-0'
+                            ],
+                            plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties']
+                        }
+                    }
+                ]   
+
+
+
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    filename: "styles-build.css",
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            }, {
+                test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
+                use: [
+                    {
+                        loader: "file-loader?name=[name].[ext]"
+                    }
+                ]
+
+            }
+        ]
+    },
+
+    plugins: debug
+        ? [extractSASS]
+        : [
+            extractSASS,
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify('production')
+                }
+            }),
+            new webpack
+                .optimize
+                .UglifyJsPlugin({})
+        ],
+    devServer: {
+        host: "localhost",
+        port: 9000,
+        contentBase: __dirname + "/build"
+    }
 };
